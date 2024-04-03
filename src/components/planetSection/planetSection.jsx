f81c5af3596d0e843c4ed11fca179b121278d7ed
Seemab Ahmed <seemab40615@gmail.com>
2024-04-04 02:11:06 +0500

    fix

---
+++ src/components/planetSection/planetSection.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function PlanetSection() {
+  return (
+    <div>
+    <div className="planetsSection">
+        <div className="textBlock">
+            <div className="titleSection">
+                <div className="outlined">
+                    Welcome to the Verified Galaxy!
+                </div>
+                <div className="regular">Welcome to the Verified Galaxy!</div>
+            </div>
+            <div className="description">
+                ExoWorlds is a Sci-Fi NFT blockchain MMORPG built on the
+                VeChainThor Blockchain. Evolve your civilization and join
+                the stars in a Galactic P2E Metaverse. Join the Galactic
+                Senate, establish planetary alliances, join Factions,
+                create super-luminal trade routes, compete with and
+                challenge other planets! Explore the Verified Galaxy!<br /><br />ExoWorlds
+                are a collection of 10,000 unique and procedurally
+                generated Planet NFT’s that give owners the rights to
+                participate in the next big Metaverse blockchain game on
+                VeChainThor.
+            </div>
+            <div className="button-group">
+                <div className="goToTrailer">
+                    <div className="button__accent button__more">
+                        <span>JOIN BETA WHITELIST</span>
+                    </div>
+                </div>
+                <div className="goToSignup">
+                    <div className="button__accent button__more">
+                        <span>SIGN UP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+  )
+}
